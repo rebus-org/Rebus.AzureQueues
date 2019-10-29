b4857ccab94c12694b363214a753211a0ec2885f
Aleksander Sleire <Alekken@live.no>
2019-10-29 11:06:02 +0100

    Add XML docs to ICloudQueueFactory

--- Rebus.AzureQueues/AzureQueues/ICloudQueueFactory.cs
+++ Rebus.AzureQueues/AzureQueues/ICloudQueueFactory.cs
@@ -7,8 +7,16 @@ using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace Rebus.AzureQueues
 {
+    /// <summary>
+    /// An abstraction to provide instances of <see cref="CloudQueue"/>
+    /// </summary>
     public interface ICloudQueueFactory
     {
+        /// <summary>
+        /// Retrieve an instance of <see cref="CloudQueue"/> targeting the given queue
+        /// </summary>
+        /// <param name="queueName">The queue to retrieve a <see cref="CloudQueue"/> for</param>
+        /// <returns></returns>
         Task<CloudQueue> GetQueue(string queueName);
     }
 }
